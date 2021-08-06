7b4763dd5309076b5e44c8d26c61e282ecb172f3
captnseagraves <k.s.seagraves@gmail.com>
2021-08-06 14:31:39 -0600

    sellTokens in progress

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -22,7 +22,7 @@ contract Vendor is Ownable {
     require(msg.value > 0, "No ETH in transaction");
 
   
-    uint256 amountOfTokens = _amountOfTokens * 10**18;
+    uint256 amountOfTokens = _amountOfTokens * yourToken.decimals();
     uint256 checkedValue = msg.value / pricePerToken;
 
     require(checkedValue == _amountOfTokens, "Must send the exact amount of ETH for amount of tokens");
@@ -33,6 +33,27 @@ contract Vendor is Ownable {
   }
 
   //ToDo: create a sellTokens() function:
+
+  function sellTokens(uint256 _amountOfTokens) public payable {
+    // require(msg.value > 0, "No ETH in transaction");
+    // require(yourToken.transfer(msg.sender, amountOfTokens));
+    // emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+
+     // decrement the token balance of the seller
+        yourToken.balances[msg.sender] -= _amountOfTokens;
+        // increment the token balance of the vendor's address. 
+        balances[] += _amount;
+
+        /*
+         * don't forget to emit the transfer event
+         * so that external apps can reflect the transfer
+         */
+        emit Transfer(msg.sender, address(this), _amount);
+        
+        // e.g. the user is selling 100 tokens, send them 500 wei
+        payable(msg.sender).transfer(amount * tokenPrice);
+  }
+
 
   //ToDo: create a withdraw() function that lets the owner, you can 
   //use the Ownable.sol import above:
