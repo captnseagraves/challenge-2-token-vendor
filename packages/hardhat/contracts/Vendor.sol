6c12ddd480bfd6e6a4a746e64e12b83fb66203e2
captnseagraves <k.s.seagraves@gmail.com>
2021-08-06 13:30:05 -0600

    buyToken working

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -2,11 +2,13 @@ pragma solidity >=0.6.0 <0.7.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
+import "hardhat/console.sol";
 
 contract Vendor is Ownable {
 
   YourToken yourToken;
   uint256 public constant tokensPerEth = 100;
+  uint256 public pricePerToken = 0.001 ether;
 
   event BuyTokens(address _buyer, uint256 _amountOfETH, uint256 _amountOfTokens);
 
@@ -16,10 +18,16 @@ contract Vendor is Ownable {
 
   //ToDo: create a payable buyTokens() function:
 
-  function buyTokens(uint256 amountOfTokens) public payable {
-    
+  function buyTokens(uint256 _amountOfTokens) public payable {
+    require(msg.value > 0, "No ETH in transaction");
 
-    transfer(msg.sender, amount of tokens)
+  
+    uint256 amountOfTokens = _amountOfTokens * 10**18;
+    uint256 checkedValue = msg.value / pricePerToken;
+
+    require(checkedValue == _amountOfTokens, "Must send the exact amount of ETH for amount of tokens");
+
+    require(yourToken.transfer(msg.sender, amountOfTokens));
 
     emit BuyTokens(msg.sender, msg.value, amountOfTokens);
   }
